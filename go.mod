8ed960fc6347680e2b9b87bda4874b36c4dce3cb
Poonam Thakur <poonamthakur1377@gmail.com>
2023-01-26 13:57:49 +0530

    initial commit to GitHub

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Poonam-13/slack-file-bot
+
+go 1.19
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/slack-go/slack v0.11.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/slack-go/slack"
+)
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4416536459077-4434592270705-uTCnnN0MuYxjWB4Gsxa2bJO3")
+	os.Setenv("CHANNEL_ID", "C04D15EHBJL")
+	api := slack.New(os.Getenv("SLACK_BOT_TOKEN"))
+	channelArr := []string{os.Getenv("CHANNEL_ID")}
+	fileArr := []string{}
+
+	for i := 0; i < len(fileArr); i++ {
+		params := slack.FileUploadParameters{
+			Channels: channelArr,
+			File:     fileArr[i],
+		}
+		file, err := api.UploadFile(params)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+			return
+		}
+		fmt.Printf("Name: %s, URL:%s\n", file.Name, file.URL)
+	}
+}
